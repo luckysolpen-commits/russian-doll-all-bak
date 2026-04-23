985bb9a41e592752444ec08011fc3bcd6e374f4e
jb m brooks <jbmbrooks1988@gmail.com>
2026-04-23 02:00:40 -0700

    sudo entity 3d

--- @.PTM.℗™/pmo-gh/1.TPMOS_c_+rmmp_84.05/#.ref/TPMOS_c_+rmmp_52.00/#.tools/check_size.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# check_size.sh - Show disk usage of files and folders
-
-cd "$(dirname "$0")/.."
-
-echo "=== TOP 20 LARGEST FILES/FOLDERS ==="
-du -ah . 2>/dev/null | sort -rh | head -n 20
--- @.PTM.℗™/pmo-gh/1.TPMOS_c_+rmmp_84.05/#.ref/TPMOS_c_+rmmp_52.00/projects/demo_1/scripts/npc_greeting.asm
+++
@@ -1,7 +0,0 @@
-# npc_greeting.asm
-# A simple dialogue tree for human testing.
-
-start:
-    console_print "Hello there! I am the project anchor."
-    console_print "Can you move the selector to that chest?"
-    halt
